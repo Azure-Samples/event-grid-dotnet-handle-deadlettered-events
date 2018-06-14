9fce871d7de9ae6324d13c951ef5e25b318fe229
Samarth <sanariel@microsoft.com>
2018-06-14 16:22:18 -0700

    first commit

---
+++ ctio/DeadLetterEvent.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using Newtonsoft.Json;
+
+//
+// Summary:
+//     Properties of an event published to an Event Grid topic.
+public class DeadLetterEvent  
+{
+    //
+    // Summary:
+    //     Initializes a new instance of the EventGridEvent class.
+    public DeadLetterEvent() { }
+    //
+    // Summary:
+    //     Initializes a new instance of the EventGridEvent class.
+    //
+    // Parameters:
+    //   id:
+    //     An unique identifier for the event.
+    //
+    //   subject:
+    //     A resource path relative to the topic path.
+    //
+    //   data:
+    //     Event data specific to the event type.
+    //
+    //   eventType:
+    //     The type of the event that occurred.
+    //
+    //   eventTime:
+    //     The time (in UTC) the event was generated.
+    //
+    //   dataVersion:
+    //     The schema version of the data object.
+    //
+    //   topic:
+    //     The resource path of the event source.
+    //
+    //   metadataVersion:
+    //     The schema version of the event metadata.
+    //
+    //   deadLetterReason:
+    //     The reason behind the message being dead lettered.
+    //         
+    //   deliveryAttempts:
+    //     The total number of delivery attempts
+    // 
+    //  lastDeliveryOutcome
+    //     The outcome of the last delivery attempt.
+    //
+    //   lastHttpStatusCode:
+    //     The response http status code when the last delivery attempt was made.
+    //
+    //
+    // Summary:
+    //     Gets or sets an unique identifier for the event.
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+    //
+    // Summary:
+    //     Gets or sets the resource path of the event source.
+    [JsonProperty(PropertyName = "topic")]
+    public string Topic { get; set; }
+    //
+    // Summary:
+    //     Gets or sets a resource path relative to the topic path.
+    [JsonProperty(PropertyName = "subject")]
+    public string Subject { get; set; }
+    //
+    // Summary:
+    //     Gets or sets event data specific to the event type.
+    [JsonProperty(PropertyName = "data")]
+    public object Data { get; set; }
+    //
+    // Summary:
+    //     Gets or sets the type of the event that occurred.
+    [JsonProperty(PropertyName = "eventType")]
+    public string EventType { get; set; }
+    //
+    // Summary:
+    //     Gets or sets the time (in UTC) the event was generated.
+    [JsonProperty(PropertyName = "eventTime")]
+    public DateTime EventTime { get; set; }
+    //
+    // Summary:
+    //     Gets the schema version of the event metadata.
+    [JsonProperty(PropertyName = "metadataVersion")]
+    public string MetadataVersion { get; }
+    //
+    // Summary:
+    //     Gets or sets the schema version of the data object.
+    [JsonProperty(PropertyName = "dataVersion")]
+    public string DataVersion { get; set; }
+
+    //Below are the dead letter specific fields
+
+    //
+    // Summary:
+    //     Gets or sets the deadLetterReason of the data object.
+    [JsonProperty(PropertyName = "deadLetterReason")]
+    public string DeadLetterReason { get; set; }
+
+    //
+    // Summary:
+    //     Gets or sets the deliveryAttempts of the data object.
+    [JsonProperty(PropertyName = "deliveryAttempts")]
+    public string DeliveryAttempts { get; set; }
+
+    //
+    // Summary:
+    //     Gets or sets the lastDeliveryOutcome of the data object.
+    [JsonProperty(PropertyName = "lastDeliveryOutcome")]
+    public string LastDeliveryOutcome { get; set; }
+
+    //
+    // Summary:
+    //     Gets or sets the lastHttpStatusCode of the data object.
+    [JsonProperty(PropertyName = "lastHttpStatusCode")]
+    public string LastHttpStatusCode { get; set; }
+
+
+}
+
---
+++ function-app/WebhookSubscriptionFunction.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DeadLetterSample
+{
+    public static class WebhookSubscriptionFunction
+    {
+        /* This function will act as an event subscription endpoint for your event subscription. The function implements the validation handshake and returns a Bad Request response
+            for all other incoming events. 
+        */
+
+        [FunctionName("WebhookSubscriptionFunction")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function begun");
+            string response = string.Empty;
+            const string SubscriptionValidationEvent = "Microsoft.EventGrid.SubscriptionValidationEvent";
+
+            string requestContent = await req.Content.ReadAsStringAsync();
+            log.Info($"Received events: {requestContent}");
+            EventGridEvent[] eventGridEvents = JsonConvert.DeserializeObject<EventGridEvent[]>(requestContent);
+
+            foreach (EventGridEvent eventGridEvent in eventGridEvents)
+            {
+                JObject dataObject = eventGridEvent.Data as JObject;
+
+                // Deserialize the event data into the appropriate type based on event type
+                if (string.Equals(eventGridEvent.EventType, SubscriptionValidationEvent, StringComparison.OrdinalIgnoreCase))
+                {
+                    var eventData = dataObject.ToObject<SubscriptionValidationEventData>();
+                    log.Info($"Got SubscriptionValidation event data, validationCode: {eventData.ValidationCode},  validationUrl: {eventData.ValidationUrl}, topic: {eventGridEvent.Topic}");
+                    // Do any additional validation (as required) such as validating that the Azure resource ID of the topic matches
+                    // the expected topic and then return back the below response
+                    var responseData = new SubscriptionValidationResponse();
+                    responseData.ValidationResponse = eventData.ValidationCode;
+                    return req.CreateResponse(HttpStatusCode.OK, responseData);
+                }
+            }
+
+            // Responding back with a 400 Bad Request is intentional and only for the purpose of demonstrating dead lettering.
+            return req.CreateResponse(HttpStatusCode.BadRequest, response);
+        }
+    }
+}
